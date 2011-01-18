7c0d1141b0f79c009733ed9ffcc23335e4cdb5ae
Zori Kostadinova zornitsa kostadinova bg <Zori Kostadinova zornitsa.kostadinova.bg@gmail.com>
2011-01-18 21:18:53 +0200

    The simple hello world nodeJS server restored.

--- test/unit/hello_world_js_server.js
+++ test/unit/hello_world_js_server.js
@@ -4,17 +4,10 @@
 // $ node hello_world_js_server.js
 
 var http = require('http');
-var io = require('socket.io');
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 });
-server.listen(3002, "127.0.0.1");
-console.log('NodeJS Server running at http://127.0.0.1:3002/');
-
-var socket = io.listen(server);
-socket.on('connection', function(client){ 
-  client.on('message', function(){ console.log("message"); return 'ham'; }); 
-  client.on('disconnect', function(){});
-}); 
+server.listen(3002, 'localhost');
+console.log('NodeJS Server running at http://localhost:3002/');
