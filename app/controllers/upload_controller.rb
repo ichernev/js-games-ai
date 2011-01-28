d6530632ebb206accd780ca2d0fc547df10d8cee
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2011-01-28 19:45:16 +0200

    Add games from the archive to the Game table in db.

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -6,7 +6,7 @@ class UploadController < ApplicationController
      flash[:notice] = 'Game package uploaded'
      redirect_to root_path
    else
-     flash[:notice] = 'Archive type should be tar'
+     flash[:notice] = 'Archive should be tar (`tar -cvf test.tar meta.json`) containing a file meta.json with the game description'
      redirect_to upload_path
    end
   end
--- app/models/package.rb
+++ app/models/package.rb
@@ -1,10 +1,13 @@
 require 'archive/tar/minitar'
+require 'json'
+
 include Archive::Tar
 
 class Package < ActiveRecord::Base
 
   @@tmp = '/tmp'
   @@dir = 'public/games'
+  @@meta_file = 'meta.json'
 
   def self.process package
     # IE gives names of uploaded files strangely. Need to be sanitized.
@@ -19,7 +22,18 @@ class Package < ActiveRecord::Base
       f.write package.read
       Minitar.unpack(tmp_path, dir_path)
     end
-    return true
+    # Read game data from meta file
+    begin
+      meta = File.join dir_path, @@meta_file
+      game_data = ''
+      File.open meta, 'r' do |f|
+        game_data = f.read
+      end
+      game = Game.new (JSON game_data)
+      return game.save
+    rescue
+      return false
+    end
   end
 
   # Delete a file from rails root.
