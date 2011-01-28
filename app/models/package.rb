090252558b1f74d861c96a7982286a263513e7e2
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2011-01-28 23:28:08 +0200

    Create css from sass files and update game if existing on unpacking uploaded archive.

--- app/models/package.rb
+++ app/models/package.rb
@@ -7,7 +7,7 @@ class Package < ActiveRecord::Base
 
   @@tmp = '/tmp'
   @@dir = 'public/games'
-  @@meta_file = 'meta.json'
+  @@meta = 'meta.json'
 
   def self.process package
     # IE gives names of uploaded files strangely. Need to be sanitized.
@@ -16,21 +16,39 @@ class Package < ActiveRecord::Base
     if content_type != 'application/x-tar' then
       return false
     end
-    tmp_path = File.join @@tmp, name
-    dir_path = File.join @@dir, File.basename(name, File.extname(name))
-    File.open tmp_path, 'wb' do |f|
-      f.write package.read
-      Minitar.unpack(tmp_path, dir_path)
-    end
-    # Read game data from meta file
     begin
-      meta = File.join dir_path, @@meta_file
+      # Unpack archive
+      tmp_path = File.join @@tmp, name
+      File.open(tmp_path, 'wb') { |f| f.write package.read }
+      Minitar.unpack(tmp_path, @@dir)
+
+      # basename, like 'Rocks'
+      basename = File.basename(name, File.extname(name))
+      # game dir, like 'public/games/Rocks'
+      game_dir = File.join @@dir, basename
+
+      # Generate .css from .sass files
+      # public/games/Rocks/sass/.. -> public/games/Rocks/css/..
+      sass = File.join game_dir, 'sass'
+      css = File.join game_dir, 'css'
+      system "sass --update #{sass}:#{css}"
+
+      # Read game data from meta file
+      meta_file = File.join game_dir, @@meta
       game_data = ''
-      File.open meta, 'r' do |f|
-        game_data = f.read
+      File.open(meta_file, 'r') { |f| game_data = f.read }
+      game_data = JSON game_data
+ 
+      # Create and save the new game, if unique
+      res = false
+      game = Game.find_by_name basename
+      if game.nil? then
+        game = Game.new game_data
+        res = game.save
+      else
+        res = game.update_attributes game_data
       end
-      game = Game.new (JSON game_data)
-      return game.save
+      return res
     rescue
       return false
     end
