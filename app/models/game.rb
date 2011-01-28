db781b75b257599b441bf952372ecc39b267f66b
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2011-01-28 18:29:31 +0200

    Changed Game model to check validness of all fields and have description accessible.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
-  attr_accessible :name, :display_name
+  attr_accessible :name, :display_name, :description
   has_many :instances
   has_many :players, :class_name => "User"
-  validates_presence_of :name
+  validates_presence_of :name, :display_name, :description
 end
